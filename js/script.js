b4db67a2e18d612c31738fdb1ec7257b8faca823
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-04-28 17:31:03 +0000

    didn't finish code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+/**
+ * This function tells you if you get a student discount
+ */
+// eslint-disable-next-line no-unused-vars
+
+function getStudentDiscount() {
+  // input
+  var userAge = document.getElementById('age-number').value
+  var daysNumber = document.getElementById('days-number').value
+
+  // process and output
+  if (day == "tuesday") || day == "thursday" || (userAge > 12 && userAge < 21)) {
+    document.getElementById('result').innerHTML = 
+    "You get the student discount for the museum."
+  }
